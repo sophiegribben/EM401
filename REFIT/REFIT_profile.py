e7a12eb4ff3c7b31e566404389d73c37024cfe8e
Gribben, Sophie <sophie.gribben@ukaea.uk>
2021-03-02 20:17:10 +0000

    creating a dictionary of starttimes for 1 hoosehold

--- REFIT/REFIT_profile.py
+++ REFIT/REFIT_profile.py
@@ -13,28 +13,40 @@ import datetime
 import os
 
 # get appliance start times
-def app_starttimes(app_num):
-    app_num = str(app_num)
+def app_starttimes():
+    #set up dictionary of start times
+    all_start_times = {}
     
-    df_on_times = df[df['Appliance'+ app_num] > 0]
+    #iterate over number of appliances
+    number = len(df.columns)-1
     
-    #extract into numpy
-    on_times = df_on_times[['Unix', 'Appliance'+ app_num]].to_numpy()
-    
-    # find the start and end values
-    #convert from unix
-    datetime_time = datetime.datetime.fromtimestamp(on_times[0, 0])
-    #initialise the array
-    start_time = np.array([datetime_time])
-    
-    for i in range(1, len(on_times)-1, 1):
-        if on_times[i+1, 1] - on_times[i, 1] > 1:
-            #convert from unix
-            datetime_time = datetime.datetime.fromtimestamp(on_times[i+1, 0])
-            #add to the array
-            start_time = np.append(start_time, datetime_time)
-    
-    return start_time
+    for app_num in range(1, number, 1):
+        app_num = str(app_num)
+        df_on_times = df[df['Appliance'+ app_num] > 0]
+        
+        #extract into numpy
+        on_times = df_on_times[['Unix', 'Appliance'+ app_num]].to_numpy()
+        
+        # find the start and end values
+        #convert from unix
+        datetime_time = datetime.datetime.fromtimestamp(on_times[0, 0])
+        #initialise the array
+        start_time = [datetime_time]
+        
+        for i in range(1, len(on_times)-1, 1):
+            if on_times[i+1, 1] - on_times[i, 1] > 1:
+                #convert from unix
+                datetime_time = datetime.datetime.fromtimestamp(on_times[i+1, 0])
+                #add to the array
+                start_time.append(datetime_time)
+        
+        #convert to numpy array - speed concerns
+        start_time = np.asarray(start_time)
+        #add to dictionary
+        all_start_times[app_num] = start_time
+        
+        
+    return all_start_times
 
 # get current working directory
 path = os.getcwd()
@@ -54,5 +66,5 @@ house = str(1)
 df = pd.read_csv(path + "/Processed_Data_CSV/House_"+ house +".csv", header=0, index_col="Time")
 
 #get the appliance data for 1 app. in 1 house
-app1_on = app_starttimes(1)
+app1_on = app_starttimes()
 
