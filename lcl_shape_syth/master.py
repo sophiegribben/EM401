7525fbc8c0b6d1be16236c0a86b2636fb9d6869d
Gribben, Sophie <sophie.gribben@ukaea.uk>
2021-03-26 15:26:35 +0000

    Added master script to run the generate year function

---
+++ lcl_shape_syth/master.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 26 15:19:45 2021
+
+@author: S. Gribben
+"""
+import guassian_model
+
+numProfiles = 300
+
+run = guassian_model.generate_year(numProfiles)
