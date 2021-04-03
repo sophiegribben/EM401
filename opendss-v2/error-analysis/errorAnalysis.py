3ce0c34b0e5e7f0a6cb27a67a21e844deaab867d
Gribben, Sophie <sophie.gribben@ukaea.uk>
2021-04-04 00:19:31 +0100

    updated error analysis scripts and files

--- opendss-v2/error-analysis/errorAnalysis.py
+++ opendss-v2/error-analysis/errorAnalysis.py
@@ -7,7 +7,10 @@ Created on Tue Mar 23 13:51:56 2021
 import numpy as np 
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error
 
+# %% error functions
 """
 MAPE calculation
 input: the actual value (LCL data) and the predicted (guassian model)
@@ -25,23 +28,66 @@ def pearsons(actual, pred):
     return np.corrcoef(actual, pred)
 
 
+#%% import data
 # get current working directory
 os.getcwd()  
 
 
 # Read the data
 lcl = pd.read_csv("lcl_feederPower.csv", header=None, index_col=0, parse_dates=True)
+lcl['HH']=lcl.index.hour*2+(lcl.index.minute/30)
+
 guassian = pd.read_csv("guassian_feederPower.csv", header=None, index_col=0, parse_dates=True)
+guassian['HH']=guassian.index.hour*2+(guassian.index.minute/30)
+
+guassian50 = pd.read_csv("guassian50_feederPower.csv", header=None, index_col=0, parse_dates=True)
+guassian50['HH']=guassian.index.hour*2+(guassian.index.minute/30)
+
 elexon = pd.read_csv("elexon_feederPower.csv", header=None, index_col=0, parse_dates=True)
+elexon['HH']=elexon.index.hour*2+(elexon.index.minute/30)
 
-# convert to numpy and flatten to get into a 1D array
-lcl = lcl.to_numpy().flatten()
-guassian = guassian.to_numpy().flatten()
-elexon = elexon.to_numpy().flatten()
-#crest = 
+crest = pd.read_csv("CREST_feederPower.csv", header=None, index_col=0, parse_dates=True)
+crest['HH']=crest.index.hour*2+(crest.index.minute/30)
 
-guassian_mape = mape(lcl, guassian)
-elexon_error = mape(lcl, elexon)
+# %% calculations
 
-guassian_rho = pearsons(lcl, guassian)
-elexon_rho = pearsons(lcl, elexon)
+#find the overall MAPE
+gua_overall = mape(lcl[1].to_numpy(), guassian[1].to_numpy())
+elexon_overall = mape(lcl[1].to_numpy(), elexon[1].to_numpy())
+crest_overall = mape(lcl[1].to_numpy(), crest[1].to_numpy())
+
+#find the mape for each HH value 
+gua_mape = np.zeros(48)
+elexon_mape = np.zeros(48)
+crest_mape = np.zeros(48)
+
+for hh in range(0, 48, 1):
+    lcl_hh = lcl.loc[(lcl['HH'] == hh)].to_numpy()
+    gua_hh = guassian.loc[(guassian['HH'] == hh)].to_numpy()
+    elexon_hh = elexon.loc[(elexon['HH'] == hh)].to_numpy()
+    crest_hh = crest.loc[(crest['HH'] == hh)].to_numpy()
+    
+    gua_mape[hh] = mape(lcl_hh[:, 0], gua_hh[:, 0])
+    elexon_mape[hh] = mape(lcl_hh[:, 0], elexon_hh[:, 0])
+    crest_mape[hh] = mape(lcl_hh[:, 0], crest_hh[:, 0])
+                            
+
+#pearsons correlation coefficent 
+guassian_rho = pearsons(lcl[1].to_numpy(), guassian[1].to_numpy())
+elexon_rho = pearsons(lcl[1].to_numpy(), elexon[1].to_numpy())
+crest_rho = pearsons(lcl[1].to_numpy(), crest[1].to_numpy())
+
+#mean squared error
+gua_mse = mean_squared_error(lcl[1].to_numpy(), guassian[1].to_numpy())
+gua50_mse = mean_squared_error(lcl[1].to_numpy(), guassian50[1].to_numpy())
+elexon_mse = mean_squared_error(lcl[1].to_numpy(), elexon[1].to_numpy())
+crest_mse = mean_squared_error(lcl[1].to_numpy(), crest[1].to_numpy())
+
+# %% plots
+#plot the error as a bar chart
+
+x = np.arange(48)
+fig, ax = plt.subplots()
+ax.bar(x, crest_mape)
+ax.set_ylabel("MAPE (%)")
+ax.set_title("MAPE of each half hour")
--- opendss-v2/error-analysis/plotting_script.py
+++ opendss-v2/error-analysis/plotting_script.py
@@ -15,12 +15,12 @@ import pandas as pd
 df_lcl = pd.read_csv("lcl_feederPower.csv", header=None, index_col=0, parse_dates=True)
 df_guassian = pd.read_csv("guassian_feederPower.csv", header=None, index_col=0, parse_dates=True)
 df_elexon = pd.read_csv("elexon_feederPower.csv", header=None, index_col=0, parse_dates=True)
+df_crest = pd.read_csv("CREST_feederPower.csv", header=None, index_col=0, parse_dates=True)
 
-#crest also needs compared
 
 
 #plot the lcl data against the models to see the correlation
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
+fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(15, 5))
 ax1.scatter(df_guassian, df_lcl)
 ax1.set_title("Guassian Model")
 ax1.set_xlabel("Actual load (kW)")
@@ -31,31 +31,41 @@ ax2.set_title("ELEXON profiles")
 ax2.set_xlabel("Actual load (kW)")
 ax2.set_ylabel("Synthesised load (kW)")
 
+ax3.scatter(df_crest, df_lcl)
+ax3.set_title("CREST Model")
+ax3.set_xlabel("Actual load (kW)")
+ax3.set_ylabel("Synthesised load (kW)")
+
 
 #plot generated profile against profile class 1 and lcl data
 fig2 = plt.figure(figsize=(10, 4))
 ax2 = plt.axes()
-ax2.plot(df_lcl['2018-01-01':'2018-01-07'], label= "LCL data")
-ax2.plot(df_guassian['2018-01-01':'2018-01-07'], label= "generated model")
-ax2.plot(df_elexon['2018-01-01':'2018-01-07'], label= "ELEXON profiles")
-ax2.set_xlabel("Date")
+ax2.plot(df_lcl['2013-01-06':'2013-01-12'].to_numpy().flatten(), label= "LCL data")
+ax2.plot(df_guassian['2018-01-01':'2018-01-07'].to_numpy().flatten(), label= "generated model")
+ax2.plot(df_elexon['2018-01-01':'2018-01-07'].to_numpy().flatten(), label= "ELEXON profiles")
+#ax2.plot(df_crest['2018-05-14':'2018-05-20'].to_numpy().flatten(), label= "CREST model")
+ax2.set_title("Feeder Load for a week in January")
+ax2.set_xlabel("Half Hour")
 ax2.set_ylabel("feeder power load (kW)")
 ax2.legend()
 
 
-#hourly boxplots
-df_lcl['Hour']=df_lcl.index.hour
-df_lcl['DayOfWeek']=df_lcl.index.dayofweek
-df_lcl['MonthOfYear']=df_lcl.index.month
-df_lcl['2018-01-01':'2018-01-30'].boxplot(column=1, by='Hour', figsize=(9,9))
+# #hourly boxplots
+# df_lcl['Hour']=df_lcl.index.hour
+# df_lcl.boxplot(column=1, by='Hour', figsize=(9,9))
 
-df_guassian['Hour']=df_guassian.index.hour
-df_guassian['DayOfWeek']=df_guassian.index.dayofweek
-df_guassian['MonthOfYear']=df_guassian.index.month
-df_guassian['2018-01-01':'2018-01-30'].boxplot(column=1, by='Hour', figsize=(9,9))
+# df_guassian['Hour']=df_guassian.index.hour
+# df_guassian.boxplot(column=1, by='Hour', figsize=(9,9))
 
-df_elexon['Hour']=df_elexon.index.hour
-df_elexon['DayOfWeek']=df_elexon.index.dayofweek
-df_elexon['MonthOfYear']=df_elexon.index.month
-df_elexon['2018-01-01':'2018-01-30'].boxplot(column=1, by='Hour', figsize=(9,9))
+# df_elexon['Hour']=df_elexon.index.hour
+# df_elexon.boxplot(column=1, by='Hour', figsize=(9,9))
 
+# df_crest['Hour']=df_crest.index.hour
+# df_crest.boxplot(column=1, by='Hour', figsize=(9,9))
+
+
+# #hourly histograms
+# df_lcl.hist(column=1, by='Hour', figsize=(12, 12))
+# df_guassian.hist(column=1, by='Hour', figsize=(12, 12))
+# df_elexon.hist(column=1, by='Hour', figsize=(12, 12))
+# df_crest.hist(column=1, by='Hour', figsize=(12, 12))
